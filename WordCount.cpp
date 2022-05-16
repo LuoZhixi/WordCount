230da3a49346b9051a0b85da2b44ce51b656ca47
LuoZhixi <2843827977@qq.com>
2022-05-16 21:39:45 +0800

    first commit

---
+++ WordCount.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<string.h>
+int main(int argc, char* argv[]) {
+	int word = 0;
+	char ch;
+	FILE* file;
+	errno_t err = fopen_s(&file,"E:\\study\\DevC++\\test.txt", "r");
+	if (err == 0) {
+		if (!strcmp(argv[1], "-c") || !strcmp(argv[1], "-C")) {
+			while ((ch = fgetc(file)) != EOF) {
+				if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+					word++;
+				}
+				else if (ch == ' ' || ch == '\t') {
+					word++;
+				}
+			}
+			printf("字符数为:%d", word);
+		}
+		else if (!strcmp(argv[1],"-w") || !strcmp(argv[1], "-W")) {
+			int flag = 0;
+			while ((ch = fgetc(file)) != EOF) {
+				if (ch == ',' || ch == ' ') {
+					if (flag == 0) {
+						word++;
+						flag = 1;
+					}
+				}
+				else {
+					flag = 0;
+				}
+			}
+			printf("单词数:%d", word);
+		}
+		if (file != NULL) {
+			fclose(file);
+		}
+	}
+}
